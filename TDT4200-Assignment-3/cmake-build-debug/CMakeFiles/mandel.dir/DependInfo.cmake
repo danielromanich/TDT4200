2c66b2ef13995873377b61c392a7e227b03848a1
danielromanich <daniel@romanich.com>
2018-10-25 12:22:27 +0200

    Add exercise 3 and 4

---
+++ TDT4200-Assignment-3/cmake-build-debug/CMakeFiles/mandel.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/daniel/Desktop/Skole/TDT4200/TDT4200/TDT4200-Assignment-3/src/main.cpp" "/home/daniel/Desktop/Skole/TDT4200/TDT4200/TDT4200-Assignment-3/cmake-build-debug/CMakeFiles/mandel.dir/src/main.cpp.o"
+  "/home/daniel/Desktop/Skole/TDT4200/TDT4200/TDT4200-Assignment-3/src/utilities/lodepng.cpp" "/home/daniel/Desktop/Skole/TDT4200/TDT4200/TDT4200-Assignment-3/cmake-build-debug/CMakeFiles/mandel.dir/src/utilities/lodepng.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "PROJECT_SOURCE_DIR=\"/home/daniel/Desktop/Skole/TDT4200/TDT4200/TDT4200-Assignment-3\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "/usr/lib/openmpi/include/openmpi/opal/mca/event/libevent2021/libevent"
+  "/usr/lib/openmpi/include/openmpi/opal/mca/event/libevent2021/libevent/include"
+  "/usr/lib/openmpi/include/openmpi"
+  "/usr/lib/openmpi/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
